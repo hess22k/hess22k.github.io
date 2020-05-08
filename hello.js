a6fda7f0afbbf8e1c5a3c5c514423b66fa777c01
hess22k <65003710+hess22k@users.noreply.github.com>
2020-05-08 02:28:21 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 function greet()
 {
   var name = document.getElementById("yourname").value;
-  document.getElementById("demo").innerHTML = "Hello, " + name + "!";
+  document.getElementById("demo").innerHTML = "Welcome to liberated radio, " + name + "!";
 }
